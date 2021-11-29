172a83fa05844304ba3c66362acc783e11f1562e
Subashini <subashini3910@gmail.com>
2021-11-29 14:02:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import BasicCard from './design';
+
+function App() {
+  return (
+    <div className="App">
+     <BasicCard />
+    </div>
+  );
+}
+
+export default App;
